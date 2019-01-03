2c8b9a5b6f68a07007749d052c395b5fbf32c42c
eks5115 <eks5115@139.com>
2019-01-03 22:13:48 +0800

    remove prod uglifyjs

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,9 +8,9 @@ module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
+    // new UglifyJSPlugin({
+    //   sourceMap: true
+    // }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     })
